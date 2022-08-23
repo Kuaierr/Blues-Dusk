6ec106718e5b8438219169f4fe046e3c37aa3c19
Luty <lutydata@gmail.com>
2022-08-23 15:40:24 +0800

    reorganize

--- Assets/GameKit/Scripts/Editor/Procedure/ScenesList.cs
+++ Assets/GameKit/Scripts/Editor/Procedure/ScenesList.cs
@@ -22,33 +22,29 @@ namespace UnityGameKit.Editor
         public static void Assets_GameKit_Demo_TaskPool_GameKitDemo_TaskPool_unity() { ScenesUpdate.OpenScene("Assets/GameKit/Demo/TaskPool/GameKitDemo_TaskPool.unity"); }
         [MenuItem("Scenes/QuickDev_TabGroup")]
         public static void Assets_GameKit_Demo_UI_TabGroup_Demo_QuickDev_TabGroup_unity() { ScenesUpdate.OpenScene("Assets/GameKit/Demo/UI/TabGroup/Demo/QuickDev_TabGroup.unity"); }
+        [MenuItem("Scenes/Demo")]
+        public static void Assets_GameMain_Arts_40_Simple_Icons_Free_Scenes_Demo_unity() { ScenesUpdate.OpenScene("Assets/GameMain/Arts/40-Simple Icons-Free/Scenes/Demo.unity"); }
+        [MenuItem("Scenes/Demo")]
+        public static void Assets_GameMain_Externals_Prototype_Kit_1_Demo_unity() { ScenesUpdate.OpenScene("Assets/GameMain/Externals/Prototype Kit 1/Demo.unity"); }
         [MenuItem("Scenes/GameMain")]
         public static void Assets_GameMain_Scenes_GameMain_unity() { ScenesUpdate.OpenScene("Assets/GameMain/Scenes/GameMain.unity"); }
         [MenuItem("Scenes/Prototype_Dialog")]
-        public static void Assets_GamePrototype_DialogSystem_Demo_Scenes_Prototype_Dialog_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/DialogSystem/Demo/Scenes/Prototype_Dialog.unity"); }
-        [MenuItem("Scenes/Prototype_Dialog_UI")]
-        public static void Assets_GamePrototype_DialogSystem_Demo_Scenes_Prototype_Dialog_UI_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/DialogSystem/Demo/Scenes/Prototype_Dialog_UI.unity"); }
+        public static void Assets_GameMain_Scenes_Prototype_Dialog_unity() { ScenesUpdate.OpenScene("Assets/GameMain/Scenes/Prototype_Dialog.unity"); }
         [MenuItem("Scenes/Prototype_Dialog_UIandDice")]
-        public static void Assets_GamePrototype_DiceSystem_Scenes_Prototype_Dialog_UIandDice_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/DiceSystem/Scenes/Prototype_Dialog_UIandDice.unity"); }
-        [MenuItem("Scenes/Prototype_Inventory")]
-        public static void Assets_GamePrototype_InventorySystem_Demo_Scenes_Prototype_Inventory_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/InventorySystem/Demo/Scenes/Prototype_Inventory.unity"); }
+        public static void Assets_GameMain_Scenes_Prototype_Dialog_UIandDice_unity() { ScenesUpdate.OpenScene("Assets/GameMain/Scenes/Prototype_Dialog_UIandDice.unity"); }
         [MenuItem("Scenes/Prototype_Interact")]
-        public static void Assets_GamePrototype_MovementSystem_Prototype_Interact_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/MovementSystem/Prototype_Interact.unity"); }
-        [MenuItem("Scenes/Addressable")]
-        public static void Assets_GamePrototype_SerializeSystem_Addressable_Scenes_Addressable_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/SerializeSystem/Addressable/Scenes/Addressable.unity"); }
+        public static void Assets_GameMain_Scenes_Prototype_Interact_unity() { ScenesUpdate.OpenScene("Assets/GameMain/Scenes/Prototype_Interact.unity"); }
+        [MenuItem("Scenes/Prototype_Inventory")]
+        public static void Assets_GameMain_Scenes_Prototype_Inventory_unity() { ScenesUpdate.OpenScene("Assets/GameMain/Scenes/Prototype_Inventory.unity"); }
         [MenuItem("Scenes/Testing")]
-        public static void Assets_GamePrototype_Testing_Testing_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/Testing/Testing.unity"); }
+        public static void Assets_GameMain_Testing_Testing_unity() { ScenesUpdate.OpenScene("Assets/GameMain/Testing/Testing.unity"); }
         [MenuItem("Scenes/Demo")]
-        public static void Assets_GamePrototype_Universal_Arts_40_Simple_Icons_Free_Scenes_Demo_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/Universal/Arts/40-Simple Icons-Free/Scenes/Demo.unity"); }
-        [MenuItem("Scenes/Demo")]
-        public static void Assets_GamePrototype_Universal_Arts_Prototype_Kit_1_Demo_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/Universal/Arts/Prototype Kit 1/Demo.unity"); }
-        [MenuItem("Scenes/Demo")]
-        public static void Assets_Resources_Model_AnimeGirls_Casual1_Demo_unity() { ScenesUpdate.OpenScene("Assets/Resources/Model/AnimeGirls/Casual1/Demo.unity"); }
+        public static void Assets_GamePrototype_Resources_Model_AnimeGirls_Casual1_Demo_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/Resources/Model/AnimeGirls/Casual1/Demo.unity"); }
         [MenuItem("Scenes/S_heya_parlor")]
-        public static void Assets_Scenes_S_heya_parlor_unity() { ScenesUpdate.OpenScene("Assets/Scenes/S_heya_parlor.unity"); }
+        public static void Assets_GamePrototype_Scenes_S_heya_parlor_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/Scenes/S_heya_parlor.unity"); }
         [MenuItem("Scenes/S_heya_toilet")]
-        public static void Assets_Scenes_S_heya_toilet_unity() { ScenesUpdate.OpenScene("Assets/Scenes/S_heya_toilet.unity"); }
+        public static void Assets_GamePrototype_Scenes_S_heya_toilet_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/Scenes/S_heya_toilet.unity"); }
         [MenuItem("Scenes/S_test")]
-        public static void Assets_Scenes_S_test_unity() { ScenesUpdate.OpenScene("Assets/Scenes/S_test.unity"); }
+        public static void Assets_GamePrototype_Scenes_S_test_unity() { ScenesUpdate.OpenScene("Assets/GamePrototype/Scenes/S_test.unity"); }
     }
 }
